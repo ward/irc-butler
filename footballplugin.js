7195497de0c3c0a3336a5ad336747b52b48c2103
Ward Muylaert <ward.muylaert@gmail.com>
2016-03-01 18:02:24 +0100

    Football - games for competition, search

--- footballplugin.js
+++ footballplugin.js
@@ -8,8 +8,13 @@ const LS = require('./livescore.js');
 
 function doCountries(client, target) {
   function callback(countries) {
-    var reply = 'Countries: ' + countries.join(', ') +
-                '. Show competitions in country with: !game -l COUNTRY.';
+    var reply = '';
+    if (countries.length === 0) {
+      reply = 'No games today. Please go out and enjoy the sun, I guess?';
+    } else {
+      reply = 'Countries: ' + countries.join(', ') +
+              '. Show competitions in country with: !game -l COUNTRY.';
+    }
     client.say(target, reply);
   }
   LS.getCountries(callback);
@@ -17,12 +22,97 @@ function doCountries(client, target) {
 
 function doCompetitions(country, client, target) {
   function callback(competitions) {
-    var reply = 'Competitions in ' + country + ': ' +
+    var reply = '';
+    if (competitions.length === 0) {
+      reply = 'No competitions with games found for ' + country + '.';
+    } else {
+      reply = 'Competitions in ' + country + ': ' +
                 competitions.join(', ') +
                 '. Show games with !game -l ' + country + '/COMPETITION.';
+    }
+
     client.say(target, reply);
   }
   LS.getCompetitions(country, callback);
+}
+
+function doGames(country, competition, client, target) {
+  function callback(games) {
+    var reply = '';
+    if (games.length === 0) {
+      reply = 'No games for ' + competition + ' in ' + country + '.';
+    } else {
+      // Rebuild full games object for gamesToString
+      var _games = {};
+      _games[country] = {};
+      _games[country][competition] = games;
+      reply = gamesToString(_games);
+    }
+
+    client.say(target, reply);
+  }
+  LS.getGames(country, competition, callback);
+}
+
+function doSearch(str, client, target) {
+  function callback(games) {
+    // Make sure not too long or short
+    var count = 0;
+    for (let country of Object.keys(games)) {
+      for (let competition of Object.keys(games[country])) {
+        count += games[country][competition].length;
+      }
+    }
+    if (count >= 20) {
+      client.say(target, 'Too many results (' + count + '). Be more specific.');
+      return;
+    }
+    if (count === 0) {
+      client.say(target, 'No results, be less specific.');
+    }
+
+    // Results if all good
+    var reply = gamesToString(games);
+    client.say(target, reply);
+  }
+  var words = str.split(/\W/);
+  LS.search(words, callback);
+}
+
+/**
+ * Changes a games object into a string ready for IRC.
+ *
+ * @param {object} games The object holding all the games.
+ */
+function gamesToString(games) {
+  var result = '';
+  for (let country of Object.keys(games)) {
+    result += formatText('<' + country.toUpperCase() + '>', 'reverse') + ' ';
+    for (let competition of Object.keys(games[country])) {
+      result += formatText('[' + competition + ']', 'bold') + ' ';
+      let _games = games[country][competition].join(' ');
+      result += _games + ' ';
+    }
+  }
+  return result;
+}
+
+/**
+ * Takes some text and formats it for IRC.
+ *
+ * @param {String} text The text to format.
+ * @param {String} how The type of formatting to apply.
+ */
+function formatText(text, how) {
+  var controlCode = '\u0003';
+  var resetCode = '\u000f';
+  if (how === 'reverse') {
+    return controlCode + '\u0016' + text + resetCode;
+  } else if (how === 'bold') {
+    return controlCode + '\u0002' + text + resetCode;
+  } else {
+    throw new Error('Incorrect formatting');
+  }
 }
 
 exports.activateOn = function(client) {
@@ -39,12 +129,12 @@ exports.activateOn = function(client) {
     let competitionmatch = message.match(/^-l (.+?)\/(.+)$/);
     if (message === '') {
       doCountries(client, to);
+    } else if (competitionmatch !== null) {
+      doGames(competitionmatch[1], competitionmatch[2], client, to);
     } else if (countrymatch !== null) {
       doCompetitions(countrymatch[1], client, to);
-    } else if (competitionmatch !== null) {
-      // List games in competition
     } else {
-      // Search and defaults
+      doSearch(message, client, to);
     }
   });
 };
