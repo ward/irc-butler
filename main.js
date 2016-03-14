4c1b816722ed9dad9d5aeafd8924512c0ad3991c
Ward Muylaert <ward.muylaert@gmail.com>
2016-03-14 11:28:56 +0100

    Fix #1. Use a config file
    Other configuration files can be added. E.g., config/production.json and
    then you simply set the NODE_ENV to production before running your
    application.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var util = require('util');
 var irc = require('irc');
+var config = require('config');
 
 var plugins = require('./plugins.js');
 
@@ -8,15 +9,19 @@ const football = require('./plugins/football.js');
 const calc = require('./plugins/calc.js');
 
 /* Start the connection. */
-var client = new irc.Client('irc.freenode.net', 'irc-butler', {
-  channels: ['#irc-butler'],
-  debug: true,
-  encoding: 'utf-8',
-  floodProtection: true,
-  userName: 'butler',
-  realName: 'Your butler for a nice IRC experience',
-  stripColors: true
-});
+var client = new irc.Client(
+  config.get('irc.network'),
+  config.get('irc.nick'),
+  {
+    channels: config.get('irc.channels'),
+    debug: true,
+    encoding: 'utf-8',
+    floodProtection: true,
+    userName: config.get('irc.userName'),
+    realName: config.get('irc.realName'),
+    stripColors: true
+  }
+);
 
 client.addListener('message#', function(nick, to, text, _raw) {
   util.log(nick, to, text);
