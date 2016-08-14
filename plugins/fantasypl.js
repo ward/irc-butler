37b90a1c0199cb130818456275263588e1718047
Ward Muylaert <ward.muylaert@gmail.com>
2016-08-14 13:54:09 +0200

    fpl: correct ranking number

--- plugins/fantasypl.js
+++ plugins/fantasypl.js
@@ -7,27 +7,13 @@
 const http = require('http');
 const https = require('https');
 
-function showNewEntries(id, client, target) {
-  let κ = function(res) {
-    let newEntries = res['new_entries']['results'].map(e => e.entry_name);
-    let amount = newEntries.length;
-    newEntries = newEntries.join(', ');
-    let name = res['league']['name'];
-    client.say(target, amount + ' teams for ' + name + ': ' + newEntries);
-  };
-  let κfail = function(err) {
-    client.say(target, 'Something went wrong --- ' + err);
-  };
-  fetchLeagueClassicStandings(id, κ, κfail);
-}
-
 function showStandings(id, client, target) {
   let κ = function(res) {
     // results is an array of the fpl teams
     let results = res['standings']['results'];
     let name = res['league']['name'];
     let entryToText = function(entry, idx) {
-      return '(' + (idx+1) + ') ' + entry.entry_name + ' ' + entry.total + 'pts';
+      return '(' + entry.rank + ') ' + entry.entry_name + ' ' + entry.total + 'pts';
     };
     let output = results.slice(0, 10).map(entryToText).join(' ');
     output = '[' + name + '] ' + output;
