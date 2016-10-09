dcfda56cb360b00c5116c04eff6ae321fc534900
Ward Muylaert <ward.muylaert@gmail.com>
2016-10-09 18:27:01 +0200

    Calc: add some shortcuts for common conversions

--- plugins/calc.js
+++ plugins/calc.js
@@ -8,6 +8,33 @@ var math = require('mathjs');
 
 const trigger = /^!calc /i;
 
+const shortcuts = [
+  {
+    trigger: /^!c ([0-9.]+)f?$/i,
+    evalString: ' degF in degC'
+  },
+  {
+    trigger: /^!f ([0-9.]+)c?$/i,
+    evalString: ' degC in degF'
+  },
+  {
+    trigger: /^!km ([0-9.]+)(?:mi)?$/i,
+    evalString: ' mile in kilometer'
+  },
+  {
+    trigger: /^!mi(?:le)? ([0-9.]+)(?:km)?$/i,
+    evalString: ' kilometer in mile'
+  },
+  {
+    trigger: /^!kg ([0-9.]+)(?:lbs)?$/i,
+    evalString: ' lbs in kilogram'
+  },
+  {
+    trigger: /^!lbs ([0-9.]+)(?:kg)?$/i,
+    evalString: ' kilogram in lbs'
+  }
+];
+
 exports.activateOn = function(client) {
   client.addListener('message#', function(from, to, text) {
     if (text.search(trigger) > -1) {
@@ -18,6 +45,21 @@ exports.activateOn = function(client) {
       } catch (e) {
         console.error(e);
         client.say(to, 'Sorry, that was a bit too hard for me.');
+      }
+    }
+
+    // Shortcuts
+    for (let i = 0; i < shortcuts.length; i++) {
+      let m = text.match(shortcuts[i].trigger);
+      if (m !== null) {
+        let expr = m[1];
+        try {
+          let ans = math.eval(m[1] + shortcuts[i].evalString);
+          client.say(to, math.format(ans));
+        } catch(e) {
+          console.error(e);
+          client.say(to, 'Sorry, that was a bit too hard for me.');
+        }
       }
     }
   });
---
+++ test/calc.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let calc = require('../plugins/calc.js');
+let fakeClient = require('./fakeClient.js').fakeClient;
+
+calc.activateOn(fakeClient);
+
+fakeClient.triggerMessage('', '', '!c 80');
+fakeClient.triggerMessage('', '', '!c 80f');
+fakeClient.triggerMessage('', '', '!c kaka');
+fakeClient.triggerMessage('', '', '!c 80.1');
+//fakeClient.triggerMessage('', '', '!c 80...1');
+fakeClient.triggerMessage('', '', '!f 20');
+
+fakeClient.triggerMessage('', '', '!km 20');
+fakeClient.triggerMessage('', '', '!mi 20');
+fakeClient.triggerMessage('', '', '!mile 20');
+
+fakeClient.triggerMessage('', '', '!kg 20');
+fakeClient.triggerMessage('', '', '!lbs 20');
