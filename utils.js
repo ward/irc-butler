661fb7ff0b123f83d80e8113d2ba727df649f856
Ward Muylaert <ward.muylaert@gmail.com>
2016-12-28 23:00:18 +0100

    Remove control char from reverse/bold irc msg
    It does not need the colour control character to make bold or reverse
    work.

--- utils.js
+++ utils.js
@@ -14,9 +14,9 @@ function formatText(text, how) {
   var controlCode = '\u0003';
   var resetCode = '\u000f';
   if (how === 'reverse') {
-    return controlCode + '\u0016' + text + resetCode;
+    return '\u0016' + text + resetCode;
   } else if (how === 'bold') {
-    return controlCode + '\u0002' + text + resetCode;
+    return '\u0002' + text + resetCode;
   } else {
     throw new Error('Incorrect formatting');
   }
