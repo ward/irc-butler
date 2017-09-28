6cf3ec776988d968e82239316721d270a2399b08
Ward Muylaert <ward.muylaert@gmail.com>
2017-09-28 20:31:47 +0200

    Finish CL/EL standings setup

--- plugins/soccerway-plug.js
+++ plugins/soccerway-plug.js
@@ -4,6 +4,20 @@
 
 const Soccerway = require('./soccerway.js').Soccerway;
 
+function oneTeamToString(team) {
+  let result =
+    team.rank +
+    '. ' +
+    team.team +
+    ' ' +
+    team.points +
+    'pts ' +
+    team.win + '-' + team.draw + '-' + team.lose +
+    ' ' +
+    team.gf + '-' + team.ga;
+  return result;
+}
+
 const cl = new Soccerway({
   'A': 'http://int.soccerway.com/international/europe/uefa-champions-league/20172018/group-stage/group-a/g11480/',
   'B': 'http://int.soccerway.com/international/europe/uefa-champions-league/20172018/group-stage/group-b/g11481/',
@@ -16,31 +30,43 @@ const cl = new Soccerway({
   'stages': 'http://int.soccerway.com/international/europe/uefa-champions-league/20172018/s14294/final-stages/'
 });
 
-const groupMatcher = /^!stand(?:ings?)? u?cl ([A-H])$/i;
+const clGroupMatcher = /^!stand(?:ings?)? u?cl ([A-H])$/i;
 
-function oneTeamToString(team) {
-  let result =
-    team.rank +
-    '. ' +
-    team.team.toUpperCase() +
-    ' ' +
-    team.points +
-    'pts ' +
-    team.win + '-' + team.draw + '-' + team.lose +
-    ' ' +
-    team.gf + '-' + team.ga;
-  return result;
-}
+const el = new Soccerway({
+  'A': 'http://int.soccerway.com/international/europe/uefa-cup/20172018/group-stage/group-a/g11411/',
+  'B': 'http://int.soccerway.com/international/europe/uefa-cup/20172018/group-stage/group-b/g11412/',
+  'C': 'http://int.soccerway.com/international/europe/uefa-cup/20172018/group-stage/group-c/g11413/',
+  'D': 'http://int.soccerway.com/international/europe/uefa-cup/20172018/group-stage/group-d/g11414/',
+  'E': 'http://int.soccerway.com/international/europe/uefa-cup/20172018/group-stage/group-e/g11415/',
+  'F': 'http://int.soccerway.com/international/europe/uefa-cup/20172018/group-stage/group-f/g11416/',
+  'G': 'http://int.soccerway.com/international/europe/uefa-cup/20172018/group-stage/group-g/g11417/',
+  'H': 'http://int.soccerway.com/international/europe/uefa-cup/20172018/group-stage/group-h/g11418/',
+  'I': 'http://int.soccerway.com/international/europe/uefa-cup/20172018/group-stage/group-i/g11419/',
+  'J': 'http://int.soccerway.com/international/europe/uefa-cup/20172018/group-stage/group-j/g11420/',
+  'K': 'http://int.soccerway.com/international/europe/uefa-cup/20172018/group-stage/group-k/g11421/',
+  'L': 'http://int.soccerway.com/international/europe/uefa-cup/20172018/group-stage/group-l/g11422/',
+  'stages': 'http://int.soccerway.com/international/europe/uefa-cup/20172018/s14115/final-stages/'
+});
+
+const elGroupMatcher = /^!stand(?:ings?)? el ([A-L])$/i;
 
 exports.activateOn = function(client) {
   client.addListener('message#', function(from, to, text) {
     let trimmedText = text.trim();
-    let groupMatch = trimmedText.match(groupMatcher);
-    if (groupMatch !== null) {
-      let group = groupMatch[1].toUpperCase();
+    let clGroupMatch = trimmedText.match(clGroupMatcher);
+    let elGroupMatch = trimmedText.match(elGroupMatcher);
+    if (clGroupMatch !== null) {
+      let group = clGroupMatch[1].toUpperCase();
       cl.syncIfNeeded(group, function() {
         let res = cl.getGroup(group);
-        res = res.ranks.map(oneTeamToString).join('; '));
+        res = res.ranks.map(oneTeamToString).join('; ');
+        client.say(to, '[GROUP ' + group + '] ' + res);
+      });
+    } else if (elGroupMatch !== null) {
+      let group = elGroupMatch[1].toUpperCase();
+      el.syncIfNeeded(group, function() {
+        let res = el.getGroup(group);
+        res = res.ranks.map(oneTeamToString).join('; ');
         client.say(to, '[GROUP ' + group + '] ' + res);
       });
     }
@@ -57,4 +83,3 @@ exports.info = {
     },
   ]
 };
-
---
+++ test/soccerway-plug.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let p = require('../plugins/soccerway-plug.js');
+let fakeClient = require('./fakeClient.js').fakeClient;
+p.activateOn(fakeClient);
+
+fakeClient.triggerMessage('', '', '!stand cl d');
+fakeClient.triggerMessage('', '', '!stand el k');
