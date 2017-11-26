9fc88aec631f6e8a4e6bc029ad28098cfc1842c1
Ward Muylaert <ward.muylaert@gmail.com>
2017-11-26 11:03:17 +0100

    Add basic logic to show (epl) league rankings
    The rest can then be added from that idea.

--- plugins/soccerway-plug.js
+++ plugins/soccerway-plug.js
@@ -30,7 +30,7 @@ const cl = new Soccerway({
   'stages': 'http://int.soccerway.com/international/europe/uefa-champions-league/20172018/s14294/final-stages/'
 });
 
-const clGroupMatcher = /^!(?:rank|stand)(?:ings?)? u?cl ([A-H])$/i;
+const clGroupMatcher = /^!(?:rank|stand)(?:ings?)? +u?cl +([A-H])$/i;
 
 const el = new Soccerway({
   'A': 'http://int.soccerway.com/international/europe/uefa-cup/20172018/group-stage/group-a/g11411/',
@@ -48,13 +48,52 @@ const el = new Soccerway({
   'stages': 'http://int.soccerway.com/international/europe/uefa-cup/20172018/s14115/final-stages/'
 });
 
-const elGroupMatcher = /^!(?:rank|stand)(?:ings?)? el ([A-L])$/i;
+const elGroupMatcher = /^!(?:rank|stand)(?:ings?)? +el +([A-L])$/i;
+
+const epl = new Soccerway({
+  '1': 'http://int.soccerway.com/national/england/premier-league/20172018/regular-season/r41547/',
+});
+const eplMatcher = /^!(?:rank|stand)(?:ings?)? +epl(?: +(.+))?$/i;
+
+/**
+ * who can be:
+ * - undefined: first 6 are picked
+ * - a (partial) team name: name is matched, position found, function called with position
+ * - a number: position to show, 5 surrounding it will also be shown
+ */
+function decideWhichToShow(ranks, who) {
+  if (who === undefined) {
+    return ranks.slice(0, 6);
+  } else if (typeof who === 'number') {
+    // Note that the number given is in a start-at-1 context
+    if (ranks.length <= 6) { return ranks; }
+    if (who >= ranks.length - 3) { return ranks.slice(-6); }
+    if (who <= 4) { return ranks.slice(0, 6); }
+    return ranks.slice(who - 4, who + 2);
+  } else if (typeof who === 'string') {
+    // Textual representation of a number?
+    if (who.match(/^\d+$/) !== null) {
+      return decideWhichToShow(ranks, parseInt(who));
+    } else {
+      // Find the team's name (rather strict matching)
+      let who_low = who.toLowerCase();
+      for (let i = 0; i < ranks.length; i++) {
+        if (ranks[i].team.toLowerCase().includes(who_low)) {
+          return decideWhichToShow(ranks, i+1);
+        }
+      }
+      // Nobody found, just show the top
+      return decideWhichToShow(ranks, 1);
+    }
+  }
+}
 
 exports.activateOn = function(client) {
   client.addListener('message#', function(from, to, text) {
     let trimmedText = text.trim();
     let clGroupMatch = trimmedText.match(clGroupMatcher);
     let elGroupMatch = trimmedText.match(elGroupMatcher);
+    let eplMatch = trimmedText.match(eplMatcher);
     if (clGroupMatch !== null) {
       let group = clGroupMatch[1].toUpperCase();
       cl.syncIfNeeded(group, function() {
@@ -68,6 +107,12 @@ exports.activateOn = function(client) {
         let res = el.getGroup(group);
         res = res.ranks.map(oneTeamToString).join('; ');
         client.say(to, '[GROUP ' + group + '] ' + res);
+      });
+    } else if (eplMatch !== null) {
+      epl.syncIfNeeded('1', function() {
+        let res = epl.getGroup('1');
+        res = decideWhichToShow(res.ranks, eplMatch[1]).map(oneTeamToString).join('; ');
+        client.say(to, '[EPL] ' + res);
       });
     }
   });
--- test/soccerway-plug.js
+++ test/soccerway-plug.js
@@ -4,5 +4,9 @@ let p = require('../plugins/soccerway-plug.js');
 let fakeClient = require('./fakeClient.js').fakeClient;
 p.activateOn(fakeClient);
 
-fakeClient.triggerMessage('', '', '!stand cl d');
-fakeClient.triggerMessage('', '', '!stand el k');
+//fakeClient.triggerMessage('', '', '!stand cl d');
+//fakeClient.triggerMessage('', '', '!stand el k');
+
+fakeClient.triggerMessage('', '', '!rank epl');
+fakeClient.triggerMessage('', '', '!rank epl bournemouth');
+fakeClient.triggerMessage('', '', '!rank epl 17');
