1ee6524aa71afc27e789a95ea41c1bf1d0ebd5ef
Ward Muylaert <ward.muylaert@gmail.com>
2018-06-15 22:03:17 +0200

    Quick hacky showing of world cup prediction
    Thrown together while watching por-spa second half. It requires a cookie
    to be set, will need to add that in production and hope it stays
    working. Future will also need some handling of failures.

---
+++ plugins/worldcuppredictor.js
@@ -0,0 +1,124 @@
+/**
+ * Fantasy Premier League plugin
+ * @module plugins/fantasypl
+ */
+'use strict';
+
+const request = require('request');
+
+let standingsCache;
+let lastUpdate = 0;
+const CACHE_TIME = 1000 * 60 * 10;
+
+/**
+ * Given the dump of fetching the standings, tries to extract relevant info.
+ */
+function parseStanding(standings) {
+  let res = [];
+  let previousMatchPosition = 0;
+  var usernameStart = standings.indexOf("<div class='user-name'>", previousMatchPosition);
+  while (usernameStart !== -1) {
+    // Search for the end of username
+    let usernameEnd = standings.indexOf('<\\/div>', usernameStart);
+    let username = standings.substring(usernameStart, usernameEnd);
+    // Search for the points
+    let pointsStart = standings.indexOf("<td class='text-center'>", usernameEnd);
+    let pointsEnd = standings.indexOf('<\\/td>', pointsStart);
+    let points = standings.substring(pointsStart, pointsEnd);
+    let result = {
+      name: parseUsername(username),
+      points: parsePoints(points)
+    };
+    res.push(result);
+    previousMatchPosition = pointsEnd;
+    usernameStart = standings.indexOf("<div class='user-name'>", previousMatchPosition);
+  }
+  return res;
+}
+
+/**
+ * Helper function for parseStanding
+ */
+function parseUsername(usernameDump) {
+  let cleaned = usernameDump.replace(/\\t/g, '').replace(/\\n/g, '');
+  let usernameStart = cleaned.indexOf('>');
+  cleaned = cleaned.substring(usernameStart+1);
+  // Clean up "captain" info too
+  if (cleaned.indexOf('<') > -1) {
+    return cleaned.substring(0, cleaned.indexOf('<'));
+  }
+  return cleaned;
+}
+
+/**
+ * Helper function for parseStanding
+ */
+function parsePoints(pointsDump) {
+  return pointsDump.replace("<td class='text-center'>", '');
+}
+
+/**
+ * Make the request to get the standings of the given league
+ *
+ * Need to set the cookie to something that works!!!
+ *
+ * TODO: Automatically log in?
+ */
+function fetchStanding(poolid, κ, κfail) {
+  if (Date.now() < lastUpdate + CACHE_TIME) {
+    κ(standingsCache);
+    return;
+  }
+  let options = {
+    url: `https://www.superbru.com/worldcup_predictor/ajax/pool_leaderboard.php?pool_id=${poolid}&edition_id=2&round_id=1`,
+    headers: {
+      'Cookie': ''
+    }
+  };
+  request(options, function(error, response, body) {
+    if (error) {
+      κfail(error);
+      return;
+    }
+    let standings = parseStanding(body);
+    standingsCache = standings;
+    lastUpdate = Date.now();
+    κ(standings);
+  });
+}
+
+function showStanding(poolid, client, target) {
+  let κ = function(standings) {
+    let standingToText = function(standing, idx) {
+      return '(' + (idx+1) + ') ' + standing.name + ' ' + standing.points + 'pts';
+    };
+    let output = standings.map(standingToText).join(' ');
+    client.say(target, output);
+  };
+  let κfail = function(err) {
+    console.warn(err);
+    client.say(target, 'Something went wrong, sorry :/');
+  };
+  fetchStanding(poolid, κ, κfail);
+}
+
+exports.activateOn = function(client) {
+  client.addListener('message#', function(from, to, message) {
+    message = message.trim();
+    if (message.search(/^!(?:predict(?:or)?|[fp]wc)$/i) > -1) {
+      // Default action. Before starting probably "new entries"
+      showStanding(11859176, client, to);
+    }
+  });
+};
+exports.info = {
+  id: 'worldcuppredictor',
+  version: '0.0.1',
+  description: 'World Cup Predictor',
+  commands: [
+    {
+      trigger: '!predict',
+      description: 'Shows the rankings for the #reddit-soccer league.'
+    }
+  ]
+};
---
+++ test/worldcuppredictor.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let fantasypl = require('../plugins/worldcuppredictor.js');
+let fakeClient = require('./fakeClient.js').fakeClient;
+
+fantasypl.activateOn(fakeClient);
+
+fakeClient.triggerMessage('', '', '!predict');
