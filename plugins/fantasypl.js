7948fc64edfb2ab54ff4eed7f362dca929d3643f
Ward Muylaert <ward.muylaert@gmail.com>
2019-08-17 20:19:17 +0200

    fpl: throw in a bunch of headers
    Hope this makes it stop throwing the 403 error. For some reason opening
    the page just once with the proper cookie set makes it work for a while
    without the cookie too. No clue what's up with that.

--- plugins/fantasypl.js
+++ plugins/fantasypl.js
@@ -30,7 +30,19 @@ function fetchLeagueClassicStandings(id, κ, κfail) {
             + id + '/standings/',
     'gzip': true,
     'headers': {
-      'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0'
+      'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0',
+      'Host': 'fantasy.premierleague.com',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+      'Accept-Language': 'en-GB,en-US;q=0.7,en;q=0.3',
+      'Accept-Encoding': 'gzip, deflate, br',
+      'DNT': '1',
+      'Connection': 'keep-alive',
+      'Upgrade-Insecure-Requests': '1',
+      'Cache-Control': 'max-age=0',
+      // For some reason I only need the cookie to "unlock" that league, then
+      // after that it opens fine without the cookie.
+      // Perhaps use request.cookie('key1=value1') instead of headers directly
+      'Cookie': '',
     }
   };
   request(opts, function(error, response, body) {
